1ae7b7f52ba72cc60bbe61be2f7a3355310da01b
longnm2000 <longnguyenmanh20052000@gmail.com>
2024-03-06 17:25:01 +0700

    feat: build login page for mobile

--- src/components/common/CustomButton.jsx
+++ src/components/common/CustomButton.jsx
@@ -6,8 +6,10 @@ function CustomButton({ imageLink, classInfo, content }) {
         className={`w-full p-4 max-w-[382px] min-h-[59px] text-sm rounded-lg ${classInfo}`}
       >
         <div className="flex items-center justify-center">
-          <img src={imageLink} alt="" />
-          <p className="w-full pe-4 font-medium">{content}</p>
+          <div className=" w-[25px] flex h-full justify-center items-center">
+            <img src={imageLink} alt="" />
+          </div>
+          <p className="w-full pe-[25px] font-medium">{content}</p>
         </div>
       </button>
     </>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,10 +4,15 @@ import googleLogo from "../assets/img/google.svg";
 import emailLogo from "../assets/img/email.svg";
 import { useState } from "react";
 import CustomButton from "../components/common/CustomButton";
+import { motion } from "framer-motion";
 function LoginPage() {
   const [show, setShow] = useState(false);
   const handleShowOption = () => {
     setShow((prev) => !prev);
+  };
+  const fadeInVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
   };
   return (
     <>
@@ -15,8 +20,14 @@ function LoginPage() {
         <div className="h-screen w-full flex justify-center items-center pb-40">
           <img src={logo} alt="Logo" />
         </div>
-        <div className=" absolute bottom-0 left-0 w-full ">
-          <div className="flex gap-5 flex-col items-center px-6">
+        <div className=" absolute bottom-0 left-0 w-full">
+          <motion.div
+            variants={fadeInVariants}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.5 }}
+            className="flex gap-5 flex-col items-center px-6"
+          >
             <CustomButton
               imageLink={appleLogo}
               classInfo={"bg-customOrange text-white border-0"}
@@ -24,19 +35,36 @@ function LoginPage() {
             />
             {show && (
               <>
-                <CustomButton
-                  imageLink={googleLogo}
-                  classInfo={"bg-white border-2"}
-                  content={"  Tiếp tục với Google"}
-                />
-                <CustomButton
-                  imageLink={emailLogo}
-                  classInfo={"bg-white border-2"}
-                  content={"  Tiếp tục với Email"}
-                />
+                <motion.div
+                  variants={fadeInVariants}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.8 }}
+                  className="w-full"
+                >
+                  <CustomButton
+                    imageLink={googleLogo}
+                    classInfo={"bg-white border-2 block mx-auto"}
+                    content={"  Tiếp tục với Google"}
+                  />
+                </motion.div>
+                <motion.div
+                  variants={fadeInVariants}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.8 }}
+                  className="w-full"
+                >
+                  {" "}
+                  <CustomButton
+                    imageLink={emailLogo}
+                    classInfo={"bg-white border-2 block mx-auto"}
+                    content={"  Tiếp tục với Email"}
+                  />
+                </motion.div>
               </>
             )}
-          </div>
+          </motion.div>
 
           {!show ? (
             <div className=" mt-4 mb-16">
@@ -48,7 +76,13 @@ function LoginPage() {
               </button>
             </div>
           ) : (
-            <p className="text-center text-xs font-light mb-8 mt-28 font-medium ">
+            <motion.p
+              variants={fadeInVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 1 }}
+              className="text-center text-xs font-light mb-8 mt-28 font-medium "
+            >
               Bằng việc tiếp tục, đồng nghĩa <br /> bạn đã đồng ý các{" "}
               <a href="#" className=" underline">
                 điều khoản
@@ -57,7 +91,7 @@ function LoginPage() {
               <a href="#" className=" underline">
                 chính sách
               </a>
-            </p>
+            </motion.p>
           )}
         </div>
       </div>
